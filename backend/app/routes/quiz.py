64369222dbaa27f6a8ddc6e14fcb728120b82c50
Siddhant Shitole <153912632+SiddhantShitole@users.noreply.github.com>
2026-07-16 20:10:02 +0530

    Update quiz.py

--- backend/app/routes/quiz.py
+++ backend/app/routes/quiz.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter
+from app.ai_modules.quiz_generator import generate_quiz
 
 router = APIRouter()
+
+@router.get("/quiz/{concept}")
+def get_quiz(concept: str):
+
+    return generate_quiz(concept)
+
 
 @router.post("/submit-quiz")
 def submit_quiz(answer: str):
 
-    if answer.lower() == "correct":
-        score = 1
-    else:
-        score = 0
+    score = 1 if answer.lower() == "correct" else 0
 
     return {"score": score}
